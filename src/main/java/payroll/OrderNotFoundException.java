852c571ab40207c9767e98f1873c514c9cea8aed
StathisKap <stathiskap75@gmail.com>
2022-08-15 06:24:37 +0100

    Finished application

---
+++ src/main/java/payroll/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package payroll;
+
+public class OrderNotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+  OrderNotFoundException(Long id) {
+    super("Could not find order" + id);
+  }
+}
